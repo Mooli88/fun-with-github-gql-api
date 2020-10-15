f065963ac0aa9d3ef721fbf959f2a33713e62c41
mooli <mooli@universlabs.co.uk>
2020-10-15 01:36:56 +0100

    feature: add dynamic query var options

--- src/hooks/useReposQuery.tsx
+++ src/hooks/useReposQuery.tsx
@@ -3,54 +3,75 @@ import { useState } from 'react';
 import { GET_REPOS } from '../graphql/schema';
 import { IReposData, IReposVars } from '../graphql/types';
 
-interface Props {
-	autoFetch?: boolean;
-	onComplete?: (id: string) => void;
+interface IQueryOptions {
+  keyword: string;
+  stars: number;
+  sortBy: 'stars' | 'forks' | 'updated' | 'help-wanted-issues';
 }
 
-const useReposQuery = ({ autoFetch, onComplete }: Props = {}) => {
-	const [loadingState, setLoadingState] = useState(false);
-	const { data, fetchMore, loading, ...options } = useQuery<{ search: IReposData }, IReposVars>(GET_REPOS, {
-		variables: {
-			query: 're language:JavaScript stars:>10000 sort:stars',
-		},
-		onCompleted({ search }) {
-			console.log('onCompleted -> search', search.edges[0].node.name);
-		},
-		onError(err) {
-			console.log('onError -> err', err);
-		},
-	});
-	const isLoading = loadingState || loading;
-
-	const loadMore = async () => {
-		const { pageInfo: currPageInfo } = data!.search;
-		setLoadingState(true);
-		await fetchMore({
-			variables: {
-				query: 're language:JavaScript stars:>10000 sort:stars',
-				cursor: currPageInfo.endCursor,
-			},
-			updateQuery: (previousResult, { fetchMoreResult }) => {
-				const newEdges = fetchMoreResult!.search.edges;
-				const { pageInfo, repositoryCount } = fetchMoreResult!.search;
-
-				return newEdges!.length
-					? {
-							search: {
-								pageInfo,
-								repositoryCount,
-								edges: [...previousResult.search.edges, ...newEdges!],
-								__typename: previousResult.search.__typename,
-							},
-					  }
-					: previousResult;
-			},
-		});
-		setLoadingState(false);
-	};
-
-	return { data, fetchMore: loadMore, loading: isLoading, ...options };
+const DEFAULT_QUERY_OPTIONS: IQueryOptions = {
+  keyword: 're',
+  stars: 10000,
+  sortBy: 'stars',
 };
 
+interface Props {
+  queryVars?: IQueryOptions;
+  onComplete?: (data: IReposData) => void;
+}
+
+function parseQueryVars(queryVars: IQueryOptions) {
+  return `${queryVars.keyword} stars:>${queryVars.stars} sort:${queryVars.sortBy} language:JavaScript`;
+}
+
+const useReposQuery = ({ onComplete, queryVars = DEFAULT_QUERY_OPTIONS }: Props = {}) => {
+  const query = parseQueryVars(queryVars);
+  const [loadingState, setLoadingState] = useState(false);
+  const { data, fetchMore, loading, ...options } = useQuery<{ search: IReposData }, IReposVars>(GET_REPOS, {
+    variables: {
+      query,
+    },
+    onCompleted({ search }) {
+      onComplete && onComplete(search);
+    },
+    onError(err) {
+      console.log('onError -> err', err);
+    },
+  });
+  const isLoading = loadingState || loading;
+
+  const refetch = async (vars: IQueryOptions = queryVars) =>
+    options.refetch({
+      query: parseQueryVars(vars),
+    });
+
+  const loadMore = async () => {
+    const { pageInfo: currPageInfo } = data!.search;
+    setLoadingState(true);
+    await fetchMore({
+      variables: {
+        query,
+        cursor: currPageInfo.endCursor,
+      },
+      updateQuery: (previousResult, { fetchMoreResult }) => {
+        const newEdges = fetchMoreResult!.search.edges;
+        const { pageInfo, repositoryCount } = fetchMoreResult!.search;
+
+        return newEdges!.length
+          ? {
+              search: {
+                pageInfo,
+                repositoryCount,
+                edges: [...previousResult.search.edges, ...newEdges!],
+                __typename: previousResult.search.__typename,
+              },
+            }
+          : previousResult;
+      },
+    });
+    setLoadingState(false);
+  };
+
+  return { ...options, data, fetchMore: loadMore, loading: isLoading, refetch };
+};
 export default useReposQuery;
